1ef5c755ad191879d337901f96f7424d6eebeb2b
rayysalcedo <rayysalcedo@gmail.com>
2026-03-02 05:26:05 +0800

    Made config dynamic for local and live environments

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import dotenv_values
 
-# This reads the .env file directly as a dictionary
 secrets = dotenv_values(".env")
 
 class Config:
@@ -9,4 +8,6 @@ class Config:
     MYSQL_USER = secrets.get('MYSQL_USER') or os.environ.get('MYSQL_USER')
     MYSQL_PASSWORD = secrets.get('DB_PASSWORD') or os.environ.get('DB_PASSWORD')
     MYSQL_DB = secrets.get('MYSQL_DB') or os.environ.get('MYSQL_DB')
-    MYSQL_PORT = int(secrets.get('MYSQL_PORT') or os.environ.get('MYSQL_PORT', 27072))
+    
+    raw_port = secrets.get('MYSQL_PORT') or os.environ.get('MYSQL_PORT', 27072)
+    MYSQL_PORT = int(raw_port)
